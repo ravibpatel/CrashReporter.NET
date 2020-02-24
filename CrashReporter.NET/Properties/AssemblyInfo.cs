86c87d509bb97178342da47492920c440433a744
Ravi Patel <ravi15@outlook.com>
2020-02-24 17:55:41 +0530

    Updated version to 1.6.

--- CrashReporter.NET/Properties/AssemblyInfo.cs
+++ CrashReporter.NET/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.9.9.0")]
-[assembly: AssemblyFileVersion("1.9.9.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
